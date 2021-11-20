864d11fcf400e6b5a4157ee0b593555c450e9929
Saksham Jaiswal <sjais973@gmail.com>
2021-11-20 10:24:21 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
+import React from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
 
+import { Room, Star, StarBorder } from "@material-ui/icons";
+// import axios from "axios";
+// import { format } from "timeago.js";
 
 function App() {
+  const [viewport, setViewport] = React.useState({
+    height: "100vh",
+    width: "100vw",
+    latitude: 28.6139,
+    longitude: 77.209,
+    zoom: 8,
+  });
+
   return (
-    <div className="App">
-     hii
-    </div>
+    <>
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAP}
+      onViewportChange={(viewport) => setViewport(viewport)}
+      mapStyle='mapbox://styles/safak/cknndpyfq268f17p53nmpwira'
+    >
+    <Marker
+    latitude={28.99029}
+    longitude={79.54945}
+    offsetLeft={-20}
+    offsetTop={-10}
+    >
+      <Room style={{fontSize:viewport.zoom*7,color:"green"}}></Room>
+    </Marker>
+    </ReactMapGL>
+    </>
   );
 }
 
